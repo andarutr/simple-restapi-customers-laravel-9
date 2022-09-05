dd3298653a939ee442927bb3ed4320e1868d96e8
Andaru Triadi <54014525+andarutr@users.noreply.github.com>
2022-09-05 13:08:38 +0700

    #1 Push

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customer = Customer::paginate(10);
+        return response()->json([
+            'data' => $customer
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customer = Customer::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'address' => $request->address,
+            'age' => $request->age,
+            'gender' => $request->gender
+
+        ]);
+
+        return response()->json([
+            'data' => $customer
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return response()->json([
+            'data' => $customer
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Customer $customer)
+    {
+        $customer->name = $request->name;
+        $customer->email = $request->email;
+        $customer->address = $request->address;
+        $customer->age = $request->age;
+        $customer->gender = $request->gender;
+        $customer->save();
+
+        return response()->json([
+            'data' => $customer
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+        return response()->json([
+            'message' => 'Data telah terhapus!'
+        ]);
+    }
+}
